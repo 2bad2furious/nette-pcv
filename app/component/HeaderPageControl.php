9d83d6c4e6232d019bd2dd510ec7ffb949903683
2bad2furious <martin.macura@educanet.cz>
2018-01-13 13:11:18 +0100

    admin bar

--- app/component/HeaderPageControl.php
+++ app/component/HeaderPageControl.php
@@ -5,6 +5,7 @@ class HeaderPageControl extends BaseControl {
     public function render(Page $page) {
         $header = $this->getHeaderManager()->getHeader($page->getLang());
         $this->template->header = $header;
+        $this->template->isLoggedIn = $this->getPresenter()->getUser()->isLoggedIn();
         $this->template->render();
     }
 }
