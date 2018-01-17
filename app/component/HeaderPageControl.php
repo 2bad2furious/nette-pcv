724a9f4645630ebd55bacc7abacdc2c1593da946
2bad2furious <martin.macura@educanet.cz>
2018-01-17 22:09:55 +0100

    Proper translation for header bar

--- app/component/HeaderPageControl.php
+++ app/component/HeaderPageControl.php
@@ -19,7 +19,14 @@ class HeaderPageControl extends BaseControl {
         $identity = $this->getPresenter()->getUser()->getIdentity();
         $this->template->username = $identity->getUsername();
         $this->template->page = $page;
-        $this->template->admin_locale = $this->getPresenter()->getCurrentAdminLocale();
+        $this->template->admin_locale = $adminLocale = $this->getPresenter()->getCurrentAdminLocale();
+
+        /* translating admin header in language set in in his identity */
+        $translator = $this->getPresenter()->translator;
+        $this->template->admin_go_edit_label = $translator->translate("edit_page", null, [], null, $adminLocale);
+        $this->template->admin_go_home_label = $translator->translate("go_admin");
+
+
         $this->template->render();
     }
 }
