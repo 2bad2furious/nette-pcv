74ab96f6b39db4777cc4263f8134b9e2e6a8d9de
Unknown <martin.macura@educanet.cz>
2018-02-09 23:06:02 +0100

    Removed unused files

--- app/component/PresentationControl.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-use Nette\Application\UI\IRenderable;
-
-abstract class PresentationControl extends BaseControl {
-    public abstract function do_render(PageWrapper $page, int $id);
-    public function render(){}
-}
