dd24f7981811185fa9a9e206a53b7b37d8a6df36
Unknown <martin.macura@educanet.cz>
2018-02-12 18:07:05 +0100

    should not depend on page

--- app/component/AdminBarControl.php
+++ app/component/AdminBarControl.php
@@ -17,19 +17,21 @@ class AdminBarControl extends BaseControl {
      * @throws InvalidState
      */
     public function render() {
-        /** @var UserIdentity $identity */
+        /** @var UserIdentity|null $identity */
         $identity = $this->getPresenter()->getUser()->getIdentity();
-        $this->template->username = $identity->getUsername();
-        $this->template->admin_locale = $adminLocale = $this->getPresenter()->getCurrentAdminLocale();
+        if ($identity instanceof UserIdentity) {
+            $this->template->username = $identity->getUsername();
+            $this->template->admin_locale = $adminLocale = $this->getPresenter()->getCurrentAdminLocale();
 
-        /* translating admin header in language set in in his identity */
-        $translator = $this->getPresenter()->translator;
-        $this->template->admin_go_home_label = $translator->translate("go_admin");
+            /* translating admin header in language set in in his identity */
+            $translator = $this->getPresenter()->translator;
+            $this->template->admin_go_home_label = $translator->translate("go_admin");
 
-        if ($this->page instanceof PageWrapper) {
-            $this->template->page = $this->page;
-            $this->template->admin_go_edit_label = $translator->translate("edit_page", null, [], null, $adminLocale);
+            if ($this->page instanceof PageWrapper) {
+                $this->template->page = $this->page;
+                $this->template->admin_go_edit_label = $translator->translate("edit_page", null, [], null, $adminLocale);
+            }
+            $this->template->render();
         }
-        $this->template->render();
     }
 }
