311ae3e880d1b668c77804db013f103a3272fbb5
Unknown <martin.macura@educanet.cz>
2018-02-13 17:57:02 +0100

    debuggin

--- public/js/js.js
+++ public/js/js.js
@@ -1,5 +1,9 @@
 _stack.push(function () {
     jQuery(document).ready(function ($) {
-
+        $(window).resize(function () {
+            console.debug($(window).innerHeight());
+            console.debug($(window).innerWidth())
+            console.debug("\n");
+        });
     })
 });
