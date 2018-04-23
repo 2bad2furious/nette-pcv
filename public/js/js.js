ee125f48f06f363196222eba446d3cfebf73e635
2bad2furious <martin.macura@educanet.cz>
2018-04-23 21:05:31 +0200

    FE smooth scroll

--- public/js/js.js
+++ public/js/js.js
@@ -1,9 +1,20 @@
 _stack.push(function () {
     jQuery(document).ready(function ($) {
-        $(window).resize(function () {
-            console.debug($(window).innerHeight());
-            console.debug($(window).innerWidth())
-            console.debug("\n");
+        $("a").on("click", function (e) {
+            var href = $(this).attr("href");
+            if (href[0] !== "#") return;
+
+            e.preventDefault();
+            var target = $(href);
+            if (target.length) {
+                var toffsetTop = $(target.children()[0]).offset().top
+
+                $("html, body").animate({
+                    scrollTop: toffsetTop
+                }, 400)
+            }
+
         });
     })
-});
+})
+;
