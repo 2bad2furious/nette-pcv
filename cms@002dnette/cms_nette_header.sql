b10ca47d51bb7a21e39047c7faea92580a57905b
2bad2furious <martin.macura@educanet.cz>
2018-04-23 21:33:27 +0200

    latest dump

--- cms@002dnette/cms_nette_header.sql
+++ cms@002dnette/cms_nette_header.sql
@@ -1,2 +1,5 @@
-INSERT INTO `cms-nette`.header (header_id, lang, page_id, parent_id, url, title, position) VALUES (1, 2, 1, 0, null, 'Domů', 0);
-INSERT INTO `cms-nette`.header (header_id, lang, page_id, parent_id, url, title, position) VALUES (2, 1, 1, 0, null, null, 0);
+INSERT INTO `cms-nette`.header (header_id, lang, page_id, parent_id, url, title, position) VALUES (1, 2, 1, null, null, 'Domů', 0);
+INSERT INTO `cms-nette`.header (header_id, lang, page_id, parent_id, url, title, position) VALUES (2, 1, 1, null, null, null, 0);
+INSERT INTO `cms-nette`.header (header_id, lang, page_id, parent_id, url, title, position) VALUES (3, 1, 1, 2, null, 'asdasd', 0);
+INSERT INTO `cms-nette`.header (header_id, lang, page_id, parent_id, url, title, position) VALUES (4, 1, 1, 1, null, 'asdad', 0);
+INSERT INTO `cms-nette`.header (header_id, lang, page_id, parent_id, url, title, position) VALUES (5, 2, null, 1, 'http://fb.com', 'fb', 0);
--- cms@002dnette/cms_nette_slider.sql
+++ cms@002dnette/cms_nette_slider.sql
@@ -1,1 +1,1 @@
-INSERT INTO `cms-nette`.slider (slider_id, title, lang_id) VALUES (1, 'Hi', 0);
+INSERT INTO `cms-nette`.slider (slider_id, title, lang_id) VALUES (1, 'Hi', null);
